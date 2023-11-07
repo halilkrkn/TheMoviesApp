238d8b8e4ac1cb7a2273d2dc1ab16c7a5f113b02
halilkrkn <Halilkrkn000@gmail.com>
2023-11-07 15:33:24 +0300

    Navigation and Screens Setup

--- app/src/main/java/com/halilkrkn/themoviesapp/MainActivity.kt
+++ app/src/main/java/com/halilkrkn/themoviesapp/MainActivity.kt
@@ -10,9 +10,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.halilkrkn.themoviesapp.navigation.graphs.SetupNavGraph
 import com.halilkrkn.themoviesapp.ui.theme.TheMoviesAppTheme
 
 class MainActivity : ComponentActivity() {
+
+    lateinit var navController: NavHostController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -22,7 +27,8 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    navController = rememberNavController()
+                    SetupNavGraph(navController = navController)
                 }
             }
         }
---
+++ app/src/main/java/com/halilkrkn/themoviesapp/navigation/graphs/BottomNavGraph.kt
@@ -0,0 +1,41 @@
+package com.halilkrkn.themoviesapp.navigation.graphs
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.halilkrkn.themoviesapp.navigation.screens.BottomBarScreen
+import com.halilkrkn.themoviesapp.navigation.util.Graphs
+import com.halilkrkn.themoviesapp.presentation.main.favorites.FavoritesScreen
+import com.halilkrkn.themoviesapp.presentation.main.search.SearchScreen
+import com.halilkrkn.themoviesapp.presentation.main.watchlist.WatchListScreen
+
+
+@Composable
+fun BottomNavGraph(
+    navController: NavHostController,
+    modifier: Modifier
+) {
+    NavHost(
+        navController = navController,
+        startDestination = BottomBarScreen.WatchlistScreen.route,
+        route = Graphs.MAIN,
+        modifier = modifier
+    ) {
+
+        composable(route = BottomBarScreen.SearchScreen.route){
+            SearchScreen(navController = navController)
+        }
+
+        composable(route = BottomBarScreen.WatchlistScreen.route){
+            WatchListScreen(navController = navController)
+        }
+
+        composable(route = BottomBarScreen.FavoritesScreen.route){
+            FavoritesScreen(navController = navController)
+        }
+        detailsNavGraph(navController = navController)
+        authNavGraph(navController = navController)
+    }
+}
