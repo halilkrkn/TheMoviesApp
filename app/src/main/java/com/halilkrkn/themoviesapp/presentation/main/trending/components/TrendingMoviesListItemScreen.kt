5d5cd50de1a4cfb10c55a966b64cb4a7f0cb2c54
halilkrkn <halilkrkn000@gmail.com>
2023-12-22 01:23:53 +0300

    Changed the Chips and Added Suggestşon Chips

--- app/src/main/java/com/halilkrkn/themoviesapp/presentation/main/trending/components/TrendingMoviesListItemScreen.kt
+++ app/src/main/java/com/halilkrkn/themoviesapp/presentation/main/trending/components/TrendingMoviesListItemScreen.kt
@@ -1,37 +1,47 @@
 package com.halilkrkn.themoviesapp.presentation.main.trending.components
 
 import android.annotation.SuppressLint
-import android.util.Log
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.*
 import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.pullrefresh.PullRefreshIndicator
+import androidx.compose.material.pullrefresh.pullRefresh
+import androidx.compose.material.pullrefresh.rememberPullRefreshState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
-import com.halilkrkn.themoviesapp.core.Constants
 import com.halilkrkn.themoviesapp.domain.model.TheTrendingMovies
 import com.halilkrkn.themoviesapp.navigation.screens.DetailsScreen
+import com.halilkrkn.themoviesapp.ui.theme.orangeColor
 import kotlinx.coroutines.*
 
+@OptIn(ExperimentalMaterialApi::class)
 @SuppressLint("UnrememberedAnimatable")
 @Composable
 fun TrendingMoviesListItemScreen(
     isRefreshing: Boolean,
     theMovies: List<TheTrendingMovies>,
     navController: NavController,
+    refresh: () -> Unit,
 ) {
-    val context = LocalContext.current
+
+    val pullRefreshState = rememberPullRefreshState(
+        refreshing = isRefreshing,
+        onRefresh = {
+            refresh()
+        }
+    )
 
     Box(
         modifier = Modifier
+            .pullRefresh(pullRefreshState)
             .fillMaxSize(),
         contentAlignment = Alignment.Center
     ) {
@@ -43,19 +53,25 @@ fun TrendingMoviesListItemScreen(
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             items(theMovies) { theMovies ->
-                if (theMovies != null) {
-                    TrendingMoviesListItem(
-                        theMovies = theMovies,
-                        modifier = Modifier.fillMaxWidth(),
-                        onItemClick = {
-                            Log.d(Constants.TAG, "DetailScreen: ${theMovies.id}")
-                            navController.navigate(
-                                DetailsScreen.Detail.route.plus("/${theMovies.id}")
-                            )
-                        }
-                    )
-                }
+                TrendingMoviesListItem(
+                    theMovies = theMovies,
+                    modifier = Modifier.fillMaxWidth(),
+                    onItemClick = {
+                        navController.navigate(
+                            DetailsScreen.Detail.route.plus("/${theMovies.id}")
+                        )
+                    }
+                )
             }
         }
+
+        PullRefreshIndicator(
+            refreshing = isRefreshing,
+            state = pullRefreshState,
+            modifier = Modifier.align(Alignment.TopCenter),
+            backgroundColor = Color.Black,
+            contentColor = orangeColor,
+            scale = true
+        )
     }
 }
